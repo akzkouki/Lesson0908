561013d0bda2f2d50b02d83630e393ad72e977a1
Zero1000 <rr500822@gmail.com>
2020-09-09 11:49:53 +0900

    偶数奇数判定機能を追加

--- kawaguchi/script.js
+++ kawaguchi/script.js
@@ -4,19 +4,26 @@ const vm = new Vue({
     data:{
         first: 0,
         second: 0,
+        isEven: "",
     },
 
     computed:{
         Adder: function(){
             if(isNaN(parseFloat(this.first)) === false && isNaN(parseFloat(this.second)) === false){
-                return parseFloat(this.first) + parseFloat(this.second);
+                let add = parseFloat(this.first) + parseFloat(this.second)
+                if(add % 2 === 0){
+                    this.isEven = "偶数"
+                }
+                else{
+                    this.isEven = "奇数"
+                }
+                return add;
             }
             else{
+                this.isEven = "";
                 return 0;
             }
         }
     },
-
-
 });
 
