22ef6d8f33de8ddcacb67edeb3f2e8954c10280e
hiralin0720 <hiralin42320720@gmail.com>
2020-09-09 10:42:40 +0900

    簡易ツイッター

--- hirano/script.js
+++ hirano/script.js
@@ -1,29 +1,12 @@
 const vm = new Vue({
     el: '#app',
     data: {
-        value: 'コマンド？',
+        message: '',
         history: [],
     },
     methods: {
-        o(){
-            this.value = 'おはよう';
-            this.history.push('おはよう');
-        },
-        k(){
-            this.value = 'こんにちは';
-            this.history.push('こんにちは');
-        },
-        s(){
-            this.value = 'さようなら';
-            this.history.push('さようなら');
-        },
-        Del(){
-            this.value = '削除';
-            this.history.shift();
-        },
-        AllDel(){
-            this.value = '全削除';
-            this.history.length = 0;
+        output(){
+            this.history.push(this.message);
         },
     },
 });
